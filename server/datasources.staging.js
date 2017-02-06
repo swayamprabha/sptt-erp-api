46910b516831cfb3a007dadde6b92cf98d7ab081
Pratheek Hegde <ptk609@gmail.com>
2017-02-06 22:01:20 +0530

    chore: print Config Vars on boot

--- server/datasources.staging.js
+++ server/datasources.staging.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-  db: {
+  mongoDs: {
     connector: 'mongodb',
     hostname: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 27017,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: 'sptt-erp-stage',
+    database: process.env.DB_NAME,
   },
 };
--- server/server.js
+++ server/server.js
@@ -14,13 +14,23 @@ app.start = function() {
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
       console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      console.log('Config Variables:');
+      console.log('-----------------');
+      console.log('HOST : ' + process.env.HOST);
+      console.log('PORT : ' + process.env.PORT);
+      console.log('NODE_ENV : ' + process.env.NODE_ENV);
+      console.log('DB_HOST : ' + process.env.DB_HOST);
+      console.log('DB_PORT : ' + process.env.DB_PORT);
+      console.log('DB_NAME : ' + process.env.DB_NAME);
+      console.log('DB_USER : ' + process.env.DB_USER);
+      console.log('DB_PASSWORD : ' + process.env.DB_PASSWORD);
     }
   });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, __dirname, function (err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`

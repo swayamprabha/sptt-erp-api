5a6c1624ebb1cf68489faaf5e654a0abdf225780
Pratheek Hegde <ptk609@gmail.com>
2017-03-27 21:47:48 +0530

    refactor: move trip-summary to daywise-summary

--- server/models/allday-summary.js
+++ server/models/allday-summary.js
@@ -1,5 +1,22 @@
 'use strict';
-
+// Add hooks for before save and before delete
 module.exports = function(Alldaysummary) {
+  // To get acccess to other models
+  // var app = require('../../server/server');
 
+  // // Hook for calculating MBG before saving the all day summary from the Trip Summary
+  // Alldaysummary.observe('before save', function(ctx, next) {
+  //   var TripSummary = app.models.TripSummary;
+  //   var MBG = app.models.MBG;
+
+  //   if (ctx.instance) {
+  //     TripSummary.find(function(err, trips) {
+  //       MBG.find(function(err, mbg) {
+  //         console.log(trips);
+  //         console.log(mbg);
+  //         next();
+  //       });
+  //     });
+  //   }
+  // });
 };
